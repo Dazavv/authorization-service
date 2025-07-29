abc286dd0aa4f9a52f1da23479b199e86050e206
dazavv <dasha7348@gmail.com>
2025-07-30 00:00:09 +0300

    updated

--- src/main/java/org/example/controller/Controller.java
+++ src/main/java/org/example/controller/Controller.java
@@ -1,4 +1,32 @@
 package org.example.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+@RequiredArgsConstructor
 public class Controller {
+    private final AuthService authServive;
+
+    @PreAuthorize("hasAuthority('USER')")
+    @GetMapping("hello/user")
+    public ResponseEntity<String> helloUser() {
+        final JwtAuthentication authInfo = authServive.getAuthInfo();
+        return ResponseEntity.ok("Hello user " + authInfo.getPrincipal() + "!");
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("hello/admin")
+    public ResponseEntity<String> helloAdmin() {
+        final JwtAuthentication authInfo = authServive.getAuthInfo();
+        return ResponseEntity.ok("Hello admin " + authInfo.getPrincipal() + "!");
+    }
+
+
 }
