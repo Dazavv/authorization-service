550aae7dec98af9b91dc4be1905f3ab564c4b636
dazavv <dasha7348@gmail.com>
2025-07-30 03:41:12 +0300

    new methods added

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -4,27 +4,29 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.example.model.Role;
 import org.example.model.User;
+import org.example.repository.UserRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final List<User> users;
-
-    public UserService() {
-        this.users = List.of(
-                new User("anton", "1234", "Антон", "Иванов", "ivanov@mail.ru", Collections.singleton(Role.USER)),
-                new User("ivan", "12345", "Сергей", "Петров", "petrov@email.com", Collections.singleton(Role.ADMIN))
-        );
-    }
+    private final UserRepository userRepository;
 
     public Optional<User> getByLogin(@NonNull String login) {
-        return users.stream()
-                .filter(user -> login.equals(user.getLogin()))
-                .findFirst();
+        return userRepository.findByLogin(login);
+    }
+
+    public boolean checkExistedUser(String login, String email) {
+        return userRepository.existsByLogin(login) || userRepository.existsByEmail(email);
+    }
+
+    public void saveNewUser(User user) {
+        userRepository.save(user);
+    }
+
+    public void addNewRole(User user, Role role) {
+        user.getRoles().add(role);
+        userRepository.save(user);
     }
 }
